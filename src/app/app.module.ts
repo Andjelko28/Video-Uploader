531e94cb355e0f1e62d0394c24c02f5cbfe917f3
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-05-30 17:15:32 +0200

    - Update app.module for starting app without errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './components/app/app.component';
+import { PagesModule } from './components/pages/pages.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent],
+  bootstrap:[AppComponent],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    PagesModule
   ]
 })
 export class AppModule { }
--- src/app/components/pages/pages-routing.module.ts
+++ src/app/components/pages/pages-routing.module.ts
@@ -6,7 +6,7 @@ import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
-    path: '/', component: HomeComponent
+    path: '', component: HomeComponent
   },
   {
     path: 'login', component: LoginComponent
