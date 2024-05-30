fc9d32fd91d986252c9c2a23b2d20e86e45c4914
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-05-30 17:07:10 +0200

    "Updated app routing and pages module to configure routes and declare components"

--- src/app/components/pages/pages.module.ts
+++ src/app/components/pages/pages.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     PagesRoutingModule
